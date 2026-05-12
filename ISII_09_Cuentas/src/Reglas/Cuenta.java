4738cdc3a790260a5c03c6c86734ab9d81ad76c7
CesarAlejandroSoto <cesar2225612@correo.uis.edu.co>
2026-05-12 13:11:54 -0500

    Implementacion Reingenieria

--- ISII_09_Cuentas/src/Reglas/Cuenta.java
+++ ISII_09_Cuentas/src/Reglas/Cuenta.java
@@ -1,27 +1,26 @@
 package Reglas;
 
-public abstract class Cuenta {
+public class Cuenta {
     protected double saldo;
 
     public Cuenta() {
-        this.saldo = 0;
+        saldo = 0;
     }
 
     public double getSaldo() {
         return saldo;
     }
 
-    public void depositar(double monto) {
-        if (monto > 0) {
-            saldo += monto;
-        }
-    }
-
-    public boolean retirar(double monto) {
-        if (monto > 0 && saldo >= monto) {
-            saldo -= monto;
+    public boolean retirar(double valor) {
+        if(valor <= saldo) {
+            saldo -= valor;
             return true;
         }
         return false;
     }
+
+    public void consignar(double valor) {
+        saldo += valor;
+    }
 }
+
--- ISII_09_Cuentas/src/Reglas/CuentaAhorros.java
+++ ISII_09_Cuentas/src/Reglas/CuentaAhorros.java
@@ -1,27 +1,37 @@
 package Reglas;
 
 public class CuentaAhorros extends Cuenta {
-    private static final double INTERES_POR_TRANSACCION = 0.01; // 1%
+
+    private static final double INTERES_POR_TRANSACCION = 0.01;
 
     @Override
-    public void depositar(double monto) {
+    public void consignar(double monto) {
+
         if (monto > 0) {
+
             saldo += monto;
+
             aplicarInteres();
         }
     }
 
     @Override
     public boolean retirar(double monto) {
+
         if (monto > 0 && saldo >= monto) {
+
             saldo -= monto;
+
             aplicarInteres();
+
             return true;
         }
+
         return false;
     }
 
     private void aplicarInteres() {
+
         saldo += saldo * INTERES_POR_TRANSACCION;
     }
 }
